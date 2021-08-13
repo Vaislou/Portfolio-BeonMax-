7cde49206820d611d6ac3ff4425afef69a4c6da0
Vaislou <vaislou@gmail.com>
2021-08-13 04:27:20 +0300

    fix: complete task 86 (4.15)

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
 	  menu = document.querySelector('.menu'),
-	  closeBtn = document.querySelector('.menu__close');
+	  closeBtn = document.querySelector('.menu__close'),
+	  percents = document.querySelectorAll('.skills__progress-percent'),
+	  fills = document.querySelectorAll('.skills__progress-bar_fill');
 
 hamburger.addEventListener('click', () => {
 	menu.classList.toggle('active');
@@ -8,4 +10,8 @@ hamburger.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
 	menu.classList.toggle('active');
+});
+
+percents.forEach((item, i) => {
+	fills[i].style.width = item.innerHTML;
 });
